1201eb419e070ecdd21d855666c666d99ee2b219
Brikena <kena97@hotmail.com>
2016-12-17 23:31:16 +0000

    added

--- app/mod_main/views.py
+++ app/mod_main/views.py
@@ -1,11 +1,21 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
+from bson import json util, ObjectId
 
 mod_main = Blueprint('main', __name__)
 
+from app import mongo
 
-@mod_main.route('/', methods=['GET'])
+@mod_main.route('/', methods=['GET', 'Post'])
 def index():
-    ''' Renders the App index page.
-    :return:
-    '''
-    return "Welcome to the Flask App."
+
+    db mongo.db.form_data
+
+    if request.method == 'GET':
+        return render_template('techstitution1.html')
+    elif request.method == 'POST':
+        data = request.form.to_dict()
+        db.insert(data)
+        # db = mongo.db.arkep.insert(request.form.to_dict())
+        return render_template('techstitution1.html')
+    else:
+        return "Bad Request"
